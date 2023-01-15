7a91550857ce5add3f9992a7c2e04b5ab77bf465
Raghunathen <raghu220805@gmail.com>
2023-01-15 19:13:48 +0530

    csv support

--- main.py
+++ main.py
@@ -4,14 +4,14 @@
 # 1. min caps, lows, symbols // menu
 # 2. store in local csv
 
-import random, string
+import random, string, csv
 
-no = 10
-word = "asd"
-alter = False
+no = 12
+word = "sakthish"
+alter = True
 altered = ""
 
-alterdict = { "a" : "@", "O" : "0", "o" : "0", "E" : "3", "e" : "3" }
+alterdict = { "a" : "@", "O" : "0", "o" : "0", "E" : "3", "e" : "3", "S" : "5", "s" : "5", "g" : "9", "I" : "1", "i" : "1" }
 
 if alter:
     for i in word:
@@ -27,14 +27,24 @@ else:
 
 a = list(string.printable)
 a.remove(" ")
-
-
-
-
+a.remove(",")
+a.remove(";")
 
 while len(key) < no:
     key += random.choice(a)
 
 
+website = "google"
+userid = "user1"
+
+f = [website, userid, key]
+
+
+save = True
+
+if save:
+    with open("asd.csv", "a") as file:
+        a = csv.writer(file)
+        a.writerow(f)
+
 print(key)
-print(len(key))
